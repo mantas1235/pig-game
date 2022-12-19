5ca7a658684f37f78d668b9d79890f876a2e57bc
Mantas Santockis <mantas.san@gmail.com>
2022-12-19 19:56:28 +0200

    first player current score

---
+++ script.js
@@ -0,0 +1,53 @@
+'use strict';
+
+
+//selecting elements
+const score0El = document.querySelector("#score--0");
+const score1El = document.getElementById("score--1");
+const current0El = document.getElementById("current--0")
+const current1El = document.getElementById("current--1")
+
+
+
+const diceEl = document.querySelector(".dice")
+const btnNew = document.querySelector(".btn--new")
+const btnRoll = document.querySelector(".btn--roll")
+const btnHold = document.querySelector(".btn--hold")
+
+
+
+score0El.textContent = 0
+score1El.textContent = 0
+diceEl.classList.add("hidden")
+
+let currentScore = 0
+
+
+btnRoll.addEventListener("click", function () {
+    //generate a random dice roll
+    const dice = Math.trunc(Math.random() * 6) + 1
+
+    //2. display dice
+    diceEl.classList.remove("hidden")
+    diceEl.src = `dice-${dice}.png`
+
+    //check for rolled 1: if true swich next player
+    if (dice !== 1) {
+        // Add dice to current score
+        currentScore += dice
+        current0El.textContent = currentScore
+
+    }
+    else {
+        //swich next player
+
+
+
+    }
+
+
+
+})
+
+
+
